581b0e2f99bca903513ae52800cd0c292f3a2bf3
fmachek <frantisekmachek2@gmail.com>
2026-03-08 19:57:28 +0100

    feat: add character spawner wave array
    Add a new PackedInt32Array to character_spawner.tscn, which can be used to set the waves during which characters should be spawned by the spawner via the inspector. Also redesign world_1.tscn: add new spawners, move the existing spawners and set the wave arrays for the first 4 waves.
    
    Currently no characters are spawned after wave 4, but that is intended.

--- scripts/objects/character_spawner.gd
+++ scripts/objects/character_spawner.gd
@@ -5,6 +5,11 @@ extends Node2D
 @export var draw_color := Color(0.447, 0.447, 0.447, 1.0)
 @export var outline_color := Color(0.352, 0.352, 0.352, 1.0)
 
+# Used for spawning in waves.
+# Allows for placement in a scene and setting the waves during which
+# the character should be spawned via the inspector.
+@export var spawn_waves: PackedInt32Array # Used for spawning in waves
+
 func _draw():
 	var radius = $Area2D/CollisionShape2D.shape.radius
 	draw_circle(Vector2.ZERO, radius, draw_color)
@@ -16,5 +21,8 @@ func spawn_character() -> void:
 	character.global_position = global_position
 	get_parent().add_child(character)
 
-func _on_wave_started() -> void:
-	spawn_character()
+# Checks if the new wave is in the spawn_waves array.
+# If it is, then a character shold be spawned.
+func _on_wave_changed(wave: int) -> void:
+	if wave in spawn_waves:
+		spawn_character()
