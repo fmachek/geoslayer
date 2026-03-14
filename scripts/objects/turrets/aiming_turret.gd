c957f46d4a8655eb98cf44374861c0bf557f0206
fmachek <frantisekmachek2@gmail.com>
2026-03-14 11:45:44 +0100

    fix: set aiming turret target to null on player death
    Connect the PlayerCharacter died signal to a function in aiming_turret.gd to ensure that after the player dies, the target is set to null and the turret stops trying to aim at the nonexistent player (this would cause a crash). Also add a target check to _process.

--- scripts/objects/turrets/aiming_turret.gd
+++ scripts/objects/turrets/aiming_turret.gd
@@ -11,9 +11,15 @@ func _ready() -> void:
 	stopped_shooting.connect(func(): is_aiming = false)
 
 func _process(delta: float) -> void:
-	if is_aiming:
+	if is_aiming and aim_target:
 		look_at(aim_target.global_position)
 		rotation += deg_to_rad(90)
 
 func _on_player_spawned(player: PlayerCharacter) -> void:
 	aim_target = player
+	player.died.connect(_on_player_died.bind(player))
+
+func _on_player_died(player: PlayerCharacter) -> void:
+	if player.died.is_connected(_on_player_died.bind(player)):
+		player.died.disconnect(_on_player_died.bind(player))
+	aim_target = null
