a429a4093f6b9010f8fcaa30d5462919f742c317
fmachek <frantisekmachek2@gmail.com>
2026-03-14 10:49:20 +0100

    docs: add documentation for character stats
    Add documentation for scripts in the scripts\characters\character_stats\ directory (character_stat.gd and health.gd).

--- scripts/characters/character_stats/character_stat.gd
+++ scripts/characters/character_stats/character_stat.gd
@@ -1,58 +1,79 @@
-# Represents a character stat such as HP, speed, damage etc.
-# This class can work with buffs and debuffs, handles maximum and current values and so on.
-
 class_name CharacterStat
 extends Node
 
-@export var stat_name: String
+## Represents a character stat such as HP, speed, damage etc.
+##
+## This class can work with buffs and debuffs and handles maximum and current values.
+##
+## Every time a [Buff] is added or removed, [member CharacterStat.total_buff] is calculated
+## and with that [member CharacterStat.max_value_after_buffs] is changed.
+## [member CharacterStat.current_value] cannot exceed [member CharacterStat.max_value_after_buffs].
 
-@export var max_value: int # Default max value
+## Name of the stat, for example "Health".
+@export var stat_name: String
+## Default maximum value before any buffs are applied.
+@export var max_value: int
+## The current value. Used for stats which have a maximum and current value such as HP.
 @export var current_value: int
-var max_value_after_buffs: int # Max value + any buffs and debuffs
+## The modified maximum value after buffs and debuffs are applied. It is the main value
+## which is usually used. For example, when speed is used for movement, this value is what is
+## really being used, not current_value.
+var max_value_after_buffs: int
+## Sum of the values of all buffs and debuffs.
+var total_buff: int = 0
+
 # Important note:
 # Max value after buffs is the value that is usually used for various calculations.
 # For example, when speed is used for movement, what is really used is this value.
 # While all of these 3 values are used for Max HP for example, for other stats
 # such as speed, 'current_value' goes unused and is technically redundant.
 # I might change the stats system in the future if this becomes annoying.
-var total_buff: int = 0 # Sum of all buffs/debuffs
 
-var buffs: Node # Node containing Buff nodes
+## Node containing all the [Buff] nodes modifying this stat.
+var buffs: Node
 
+## Emitted when the max value changes. Passes the old and new values as arguments.
 signal max_value_changed(old_value: int, new_value: int)
+## Emitted when the current value changes. Passes the old and new values as arguments.
 signal current_value_changed(old_value: int, new_value: int)
+## Emitted when the maximum value after buffschanges. Passes the old and new values as arguments.
 signal max_value_after_buffs_changed(old_value: int, new_value: int)
+## Emitted when the total buff value changes. Passes the old and new values as arguments.
 signal total_buff_changed(old_value: int, new_value: int)
-
+## Emitted when a new [Buff] is applied to this [CharacterStat].
 signal added_buff(buff: Buff)
 
-func _ready():
+# Creates the buffs node and sets max_value_after_buffs to its default value.
+func _ready() -> void:
 	buffs = Node.new()
 	buffs.name = "Buffs"
 	add_child(buffs)
 	max_value_after_buffs = max_value
 
-# Adds a new buff to the list and calculates the total buff and applies it
+## Adds a new [Buff] to [member CharacterStat.buffs], calculates
+## [member CharacterStat.total_buff] and applies it.
 func add_buff(buff: Buff) -> void:
 	buffs.add_child(buff)
 	added_buff.emit(buff)
 	calculate_total_buff()
 
-# Removes a buff from the list and calculates the total buff and applies it
+## Removes a [Buff] from [member CharacterStat.buffs], calculates
+## [member CharacterStat.total_buff] and applies it.
 func remove_buff(buff: Buff) -> void:
 	buffs.remove_child(buff)
 	buff.queue_free()
 	calculate_total_buff()
 
-# Calculates the total buff and applies it
-func calculate_total_buff():
+## Calculates [member CharacterStat.total_buff] and applies it.
+func calculate_total_buff() -> void:
 	var buff_sum = 0
 	for buff: Buff in buffs.get_children():
 		buff_sum += buff.amount
 	change_total_buff(buff_sum)
 
-# Changes the current value, checks for edge cases such as the new value
-# exceeding the max value after buffs.
+## Changes [member CharacterStat.current_value], checks for edge cases such as the new value
+## exceeding [member CharacterStat.max_value_after_buffs].[br]
+## Emits [member CharacterStat.current_value_changed].
 func change_current_value(value: int) -> void:
 	if current_value == value:
 		return
@@ -64,7 +85,8 @@ func change_current_value(value: int) -> void:
 	current_value = value
 	current_value_changed.emit(old_value, current_value)
 
-# Changes the max value and recalculates the buff.
+## Changes [member CharacterStat.max_value] and recalculates [member CharacterStat.total_buff].[br]
+## Emits [member CharacterStat.max_value_changed].
 func change_max_value(value: int) -> void:
 	if max_value == value:
 		return
@@ -73,7 +95,11 @@ func change_max_value(value: int) -> void:
 	max_value_changed.emit(old_value, max_value)
 	calculate_total_buff()
 
-# Changes the max value after buffs
+## Changes [member CharacterStat.max_value_after_buffs]. Checks for edge cases such as
+## the new value being negative, which can happen after applying debuffs.[br]
+## Also checks if [member CharacterStat.current_value] exceeds the new
+## [member CharacterStat.max_value_after_buffs].[br]
+## Emits [member CharacterStat.max_value_after_buffs_changed].
 func change_max_value_after_buffs(value: int) -> void:
 	if value < 0:
 		value = 0
@@ -83,11 +109,12 @@ func change_max_value_after_buffs(value: int) -> void:
 	max_value_after_buffs = value
 	max_value_after_buffs_changed.emit(old_value, max_value_after_buffs)
 	
-	# Check if current value exceeds max value (can happen after debuffs)
 	if current_value > max_value_after_buffs:
 		change_current_value(max_value_after_buffs)
 
-# Changes the total buff
+## Changes [member CharacterStat.total_buff] and
+## changes [member CharacterStat.max_value_after_buffs].[br]
+## Emits [member CharacterStat.total_buff_changed].
 func change_total_buff(value: int) -> void:
 	if total_buff != value:
 		var old_value = total_buff
@@ -96,6 +123,6 @@ func change_total_buff(value: int) -> void:
 	
 	change_max_value_after_buffs(max_value + total_buff)
 
-# Adds an amount to the current value
+## Adds a given [param amount] to [member CharacterStat.current_value].
 func add_value(value: int) -> void:
 	change_current_value(current_value + value)
--- scripts/characters/character_stats/health.gd
+++ scripts/characters/character_stats/health.gd
@@ -1,38 +1,45 @@
-# Represents a character's HP. Contains regeneration logic.
-
 class_name Health
 extends CharacterStat
 
-var regen_tick_timer: Timer # Timer for regeneration ticks
-var regen_start_timer: Timer # Timer for regeneration cooldown
+## Represents a character's HP. Contains regeneration logic.
 
-@export var regen_amount: int = 2 # Heal amount each tick
+## Timer used to time regeneration ticks.
+var regen_tick_timer: Timer
+## Timer used to time the regeneration cooldown.
+var regen_start_timer: Timer
+## Heal amount each tick.
+@export var regen_amount: int = 2
 
 func _ready() -> void:
 	super()
 	regen_tick_timer = $RegenTickTimer
 	regen_start_timer = $RegenStartTimer
 
-# Handles current HP changes - if damage was taken, healing is interrupted.
+## Reacts to [member Health.current_value] changes. If damage was taken,
+## regeneration is interrupted.
 func _on_current_value_changed(old_value: int, new_value: int) -> void:
 	if old_value > new_value: # Damage was taken
 		regen_tick_timer.stop() # Stop healing
 		regen_start_timer.start() # Start cooldown for healing
 
-# When the regeneration cooldown timer times out, the tick timer starts.
+## When [member Health.regen_start_timer] times out, [member Health.regen_tick_timer] starts.
 func _on_regen_start_timer_timeout() -> void:
 	regen_tick_timer.start()
 
-# When the regeneration tick timer times out, the heal amount is added to the current value.
-# If the player hits max health, the regeneration tick timer stops.
+## When [member Health.regen_tick_timer] times out, [member Health.regen_amount] is
+## added to [member Health.current_value].[br]
+## If [member Health.current_value] has reached [member Health.max_value_after_buffs],
+## [member Health.regen_tick_timer] stops (regeneration stops).
 func _on_regen_tick_timer_timeout() -> void:
 	add_value(regen_amount)
 	if current_value == max_value_after_buffs:
 		regen_tick_timer.stop()
 
-# Handles max HP after buffs changes. If the max HP was buffed, the regeneration
-# starts immediately, so that the current HP can catch up to the max HP.
+## Reacts to [member Health.max_value_after_buffs] changes.
+## If the maximum HP was buffed, regeneration starts immediately
+## ([member Health.regen_tick_timer] starts), so that [member Health.current_value]
+## can catch up to [member Health.max_value_after_buffs].
 func _on_max_value_after_buffs_changed(old_value: int, new_value: int) -> void:
-	if new_value > old_value: # Start 
+	if new_value > old_value: # Start regeneration if max HP was buffed
 		regen_start_timer.stop()
 		regen_tick_timer.start()
