664685e236ba9a4c3beb9c6b474351c7bc26dd1d
fmachek <frantisekmachek2@gmail.com>
2026-03-20 20:46:55 +0100

    refactor: change type in connect_to_ability
    Change the type of the ability parameter in connect_to_ability in teleport_cast_particles.gd and teleport_position_particles.gd from Teleport to Ability. Also connect to the ability's character tree_exited signal in teleport_position_particles.gd to ensure that the particles disappear if the caster exits the tree while casting.

--- scripts/particle_effects/teleport_cast_particles.gd
+++ scripts/particle_effects/teleport_cast_particles.gd
@@ -6,5 +6,5 @@ func _on_finished() -> void:
 	queue_free()
 
 
-func connect_to_ability(ability: Teleport) -> void:
+func connect_to_ability(ability: Ability) -> void:
 	ability.finished_casting.connect(func(): emitting = false)
--- scripts/particle_effects/teleport_position_particles.gd
+++ scripts/particle_effects/teleport_position_particles.gd
@@ -6,5 +6,6 @@ func _on_finished() -> void:
 	queue_free()
 
 
-func connect_to_ability(ability: Teleport) -> void:
+func connect_to_ability(ability: Ability) -> void:
 	ability.finished_casting.connect(func(): emitting = false)
+	ability.character.tree_exited.connect(func(): emitting = false)
