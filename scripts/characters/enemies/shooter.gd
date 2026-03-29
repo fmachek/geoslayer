20c19e8e70164f187e674aefa49d61db5e543221
fmachek <frantisekmachek2@gmail.com>
2026-03-29 17:22:24 +0200

    feat: add Angleshot ability to Shooter
    Add the Angleshot ability to the Shooter enemy's kit to make their shooting pattern more difficult to dodge in large numbers.

--- scripts/characters/enemies/shooter.gd
+++ scripts/characters/enemies/shooter.gd
@@ -1,7 +1,8 @@
 class_name Shooter
 extends Enemy
 
-## Represents an enemy who casts [Shoot].
+## Represents an enemy who casts [Shoot] and [Angleshot].
 
 func _load_abilities() -> void:
 	_load_ability(Shoot.new())
+	_load_ability(Angleshot.new())
