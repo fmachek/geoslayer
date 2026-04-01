e8345a6c7713cce920b2aebc3aaeb74d47cbf0b0
fmachek <frantisekmachek2@gmail.com>
2026-04-01 21:50:52 +0200

    feat: add projectile with damage falloff
    Add new Projectile type: FalloffProjectile. Its damage decreases every physics frame.

---
+++ scripts/objects/projectiles/falloff_projectile.gd
@@ -0,0 +1,23 @@
+class_name FalloffProjectile
+extends Projectile
+## Type of [Projectile] whose damage decreases every physics frame.
+
+## Minimum damage the [Projectile] must deal.
+var min_damage: int = 2
+## Used when calculating damage reduction every physics frame.
+var damage_reduction_multiplier: float = 30.0
+var _damage_reduction_buildup: float = 0.0
+
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	_reduce_damage(delta)
+
+
+func _reduce_damage(delta: float) -> void:
+	_damage_reduction_buildup += delta * damage_reduction_multiplier
+	var dmg_threshold: int = 1
+	if _damage_reduction_buildup >= dmg_threshold:
+		if projectile_properties.damage - dmg_threshold > min_damage:
+			_damage_reduction_buildup -= dmg_threshold
+			projectile_properties.damage -= dmg_threshold
