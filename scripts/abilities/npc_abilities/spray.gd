28cc34174ae6aacd2b99324c2118a5cfd82a0379
fmachek <frantisekmachek2@gmail.com>
2026-04-03 23:45:49 +0200

    feat: add Sprayer enemy
    Add a new enemy type: Sprayer. It casts its own new Spray ability which fires DoT projectiles. It also explodes into many DoT projectiles when it gets close to its target and dies.

---
+++ scripts/abilities/npc_abilities/spray.gd
@@ -0,0 +1,97 @@
+class_name Spray
+extends Ability
+## Represents the Spray ability which fires multiple [DoTProjectile]s in
+## quick succession.
+##
+## Two projectiles are fired at once, at opposing sides of the attack pattern.
+## The projectiles are also slower the later they are fired.
+
+const _PROJ_SCENE := preload("res://scenes/objects/projectiles/dot_projectile.tscn")
+
+## Base speed of the [DoTProjectile]s before a speed decrease is applied.
+var projectile_speed: float = 3
+## Base damage of the [DoTProjectile]s.
+var base_damage: int = 5
+## Radius of the [DoTProjectile]s.
+var projectile_radius: int = 6
+## Amount of [DoTProjectile]s fired on each cast.
+var projectile_amount: int = 9
+## Time between firing individual [DoTProjectile]s, in seconds.
+var fire_time: float = 0.1
+## Spread of the attack pattern in radians.
+var spread: float = deg_to_rad(90)
+## Damage dealt by [member DoTProjectile.dot] on every tick.
+var dot_damage: int = 1
+## Time between individual [member DoTProjectile.dot] ticks.
+var dot_tick_time: float = 1.0
+## Total amount of [member DoTProjectile.dot] ticks.
+var dot_tick_amount: int = 5
+
+var _fire_timer: Timer
+var _proj_angles: Array[float] = []
+var _proj_speed_decrease: float
+
+
+func _init() -> void:
+	super(2, "Fires projectiles which deal damage over time.")
+
+
+func _ready() -> void:
+	_create_fire_timer()
+
+
+func _perform_ability() -> void:
+	_proj_speed_decrease = 0
+	_generate_angles()
+	_fire_timer.start()
+	_fire_at_angles()
+
+
+func _fire_projectile(angle: float) -> void:
+	var proj: DoTProjectile = ProjectileFunctions.fire_projectile_at_angle(
+			_PROJ_SCENE, angle, character, base_damage,
+			(projectile_speed - _proj_speed_decrease), projectile_radius
+	)
+	var dot := DamageOverTime.new(dot_damage, dot_tick_time, dot_tick_amount)
+	proj.dot = dot
+
+
+func _fire_at_angles() -> void:
+	_proj_speed_decrease += 0.2
+	if len(_proj_angles) > 1:
+		var angle_1: float = _proj_angles.pop_front()
+		var angle_2: float = _proj_angles.pop_back()
+		_fire_projectile(angle_1)
+		_fire_projectile(angle_2)
+	elif len(_proj_angles) > 0:
+		var angle: float = _proj_angles.pop_front()
+		_fire_projectile(angle)
+	else:
+		_finish_firing()
+
+
+func _finish_firing() -> void:
+	_fire_timer.stop()
+	finished_casting.emit()
+
+
+func _create_fire_timer() -> void:
+	_fire_timer = Timer.new()
+	_fire_timer.wait_time = fire_time
+	_fire_timer.timeout.connect(_fire_at_angles)
+	add_child(_fire_timer)
+
+
+func _reset_ability() -> void:
+	if _fire_timer:
+		_fire_timer.stop()
+
+
+func _generate_angles() -> void:
+	var target_pos: Vector2 = character.target_pos
+	var target_dir: Vector2 = character.global_position.direction_to(target_pos)
+	var target_angle: float = target_dir.angle()
+	var start_angle: float = target_angle - spread / 2
+	_proj_angles.clear()
+	for i in range(projectile_amount):
+		_proj_angles.append(start_angle + i * (spread / projectile_amount))
---
+++ scripts/characters/enemies/sprayer.gd
@@ -0,0 +1,62 @@
+class_name Sprayer
+extends Enemy
+## Represents an [Enemy] who casts [Spray] and explodes into [DoTProjectile]s
+## when it gets close to its target.
+
+const _PROJ_SCENE := preload("res://scenes/objects/projectiles/dot_projectile.tscn")
+
+## Damage dealt by [DoTProjectile]s fired when exploding.
+var proj_damage: int = 5
+## Travel speed of [DoTProjectile]s fired when exploding.
+var proj_speed: float = 3.0
+## Variety in the travel speed of [DoTProjectile]s fired when exploding.
+var proj_speed_variety: float = 0.5
+## Radius of [DoTProjectile]s fired when exploding.
+var proj_radius: int = 8
+## Amount of [DoTProjectile]s fired when exploding.
+var proj_amount: int = 40
+## Damage dealt by [DamageOverTime] applied by [DoTProjectile]s
+## fired when exploding.
+var dot_damage: int = 1
+## Time between damage ticks of [DamageOverTime] applied by
+## [DoTProjectile]s fired when exploding.
+var dot_tick_time: float = 1.0
+## Total amount of damage ticks of [DamageOverTime] applied by
+## [DoTProjectile]s fired when exploding.
+var dot_tick_amount: int = 5
+
+
+func _init() -> void:
+	# Set lower stop distance than default
+	stop_distance = 120.0
+
+
+func _ready() -> void:
+	super()
+	target_reached.connect(explode) # Explodes when it gets close
+
+
+func _load_abilities() -> void:
+	_load_ability(Spray.new())
+
+
+## Explodes into many [DoTProjectile]s and dies.
+func explode() -> void:
+	var angle: float = 0.0
+	for i in range(proj_amount):
+		angle = i * (TAU / proj_amount)
+		var direction := Vector2.from_angle(angle)
+		_fire_projectile(direction)
+	die()
+
+
+func _fire_projectile(direction: Vector2) -> void:
+	var speed_variety: float = randf_range(-proj_speed_variety, proj_speed_variety)
+	var final_speed: float = proj_speed + speed_variety
+	var props := ProjectileProperties.new(
+			draw_color, outline_color, direction, final_speed,
+			self, proj_damage, proj_radius, global_position
+	)
+	var proj: DoTProjectile = ProjectileFunctions.fire_projectile(_PROJ_SCENE, props)
+	var dot := DamageOverTime.new(dot_damage, dot_tick_time, dot_tick_amount)
+	proj.dot = dot
