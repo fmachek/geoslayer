e99516b85d14b199d49deb697e0ab5d96e89d858
fmachek <frantisekmachek2@gmail.com>
2026-04-18 01:58:40 +0200

    fix: ability texture loading
    The ability textures were being loaded incorrectly. The path requires a ".png" extension at the end.

--- scripts/user_interface/texture_manager.gd
+++ scripts/user_interface/texture_manager.gd
@@ -10,7 +10,7 @@ static var _ability_icon_dir := "res://assets/user_interface/abilities/"
 ## Returns a path to an ability icon if found. Otherwise returns
 ## a path to a placeholder texture.
 static func get_ability_icon_path(ability_name: String) -> String:
-	var ability_icon_path := _ability_icon_dir + ability_name.to_lower()
+	var ability_icon_path := _ability_icon_dir + ability_name.to_lower() + ".png"
 	if ResourceLoader.exists(ability_icon_path):
 		return ability_icon_path
 	else:
