ea206c876263bdf2d8acc9c9e8b088fc23bd9d11
fmachek <frantisekmachek2@gmail.com>
2026-05-03 22:00:34 +0200

    feat: prevent accidental death UI clicks
    Add a new timer to the UI which shows up when the player dies. For 0.5 sec after the UI is shown, the buttons don't do anything. This prevents accidental clicks, for example when the player is clicking in the middle of the screen to cast an ability and dies.

--- scripts/user_interface/death_user_interface/death_user_interface.gd
+++ scripts/user_interface/death_user_interface/death_user_interface.gd
@@ -1,16 +1,23 @@
 class_name DeathUserInterface
 extends Control
 ## Represents a UI screen which appears when the player dies.
+## The buttons don't do anything for a short period of time after
+## it is shown to prevent accidental clicks.
 
 @onready var _restart_button: Button = %RestartButton
 @onready var _menu_button: Button = %BackToMenuButton
+@onready var _enable_timer: Timer = $ClickEnableTimer
 
 
 func _ready() -> void:
 	PlayerManager.player_died.connect(_on_player_died)
-	_restart_button.pressed.connect(GameManager.start_game)
-	_menu_button.pressed.connect(GameManager.switch_to_menu)
 
 
 func _on_player_died(player: PlayerCharacter) -> void:
 	show()
+	_enable_timer.start()
+
+
+func _on_click_enable_timer_timeout() -> void:
+	_restart_button.pressed.connect(GameManager.start_game)
+	_menu_button.pressed.connect(GameManager.switch_to_menu)
