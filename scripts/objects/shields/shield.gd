843fb8b500256a5eaa8c89436cd4838c060887c0
fmachek <frantisekmachek2@gmail.com>
2026-06-01 20:56:23 +0200

    feat: add bars and particles to shields
    Add a progress bar which displays shield durability and a progress bar which displays the expiration time.
    
    Also add particles which show up when the shield is destroyed or when it expires.

--- scripts/objects/shields/shield.gd
+++ scripts/objects/shields/shield.gd
@@ -12,6 +12,9 @@ signal expiration_time_changed(new_time: float)
 ## Emitted when [member durability] changes.
 signal durability_changed(new_durability: int)
 
+const destroy_particles_scene := preload(
+	"res://scenes/particle_effects/shield_destroy_particles.tscn"
+)
 ## Fill color of the [Shield].
 @export var draw_color := Color(0.565, 0.565, 0.565, 0.25)
 ## Outline color of the [Shield].
@@ -28,10 +31,13 @@ var is_active: bool = true
 var piercing_objects: Array[Node2D] = []
 # Tween used for fade in and fade out.
 var _fade_tween: Tween
+var _time_bar_tween: Tween
 
 @onready var _area: Area2D = $Area2D
 @onready var _col_shape: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var _expiration_timer: Timer = $ExpirationTimer
+@onready var _durability_bar: ProgressBar = $BarContainer/DurabilityBarContainer/DurabilityBar
+@onready var _time_bar: ProgressBar = $BarContainer/TimeBarContainer/TimeBar
 
 
 func _ready() -> void:
@@ -40,9 +46,15 @@ func _ready() -> void:
 	durability_changed.connect(_on_durability_changed)
 	_expiration_timer.timeout.connect(destroy)
 	expiration_time_changed.connect(_update_expiration_timer)
+	durability_changed.connect(_update_durability_bar)
 	_update_shape_radius(radius)
 	_update_expiration_timer(expiration_time)
 	_update_colors()
+	_durability_bar.max_value = durability
+	_durability_bar.value = durability
+	_update_bar_positions()
+	_time_bar.value = 0
+	_tween_time_bar()
 	_expiration_timer.start()
 	_fade_in()
 
@@ -60,6 +72,7 @@ func destroy() -> void:
 	is_active = false
 	_expiration_timer.stop()
 	call_deferred("_stop_monitoring")
+	_spawn_destroy_particles()
 	_fade_out()
 
 
@@ -83,6 +96,8 @@ func _on_area_entered(area: Area2D) -> void:
 
 func _on_durability_changed(new_value: int) -> void:
 	if new_value == 0:
+		if _time_bar_tween:
+			_time_bar_tween.kill()
 		destroy()
 
 
@@ -104,6 +119,14 @@ func _is_enemy_projectile(projectile: Projectile) -> bool:
 func _stop_monitoring() -> void:
 	_area.monitoring = false
 	_area.monitorable = false
+
+
+func _spawn_destroy_particles() -> void:
+	var particles: FreeParticles = destroy_particles_scene.instantiate()
+	particles.color = outline_color
+	get_parent().add_child(particles)
+	particles.global_position = global_position
+	particles.emitting = true
 
 
 #region fades
@@ -146,6 +169,26 @@ func _update_colors() -> void:
 		if parent is Character:
 			draw_color = Color(parent.draw_color, 0.25)
 			outline_color = Color(parent.draw_color, 0.5)
+			var stylebox1: StyleBoxFlat = _durability_bar.get_theme_stylebox("fill")
+			var bar_color: Color = parent.draw_color
+			stylebox1.bg_color = bar_color
+			var stylebox2: StyleBoxFlat = _time_bar.get_theme_stylebox("fill")
+			stylebox2.bg_color = bar_color
+			var durability_texture: TextureRect = $BarContainer/DurabilityBarContainer/TextureRect
+			durability_texture.modulate = bar_color
+			var time_texture: TextureRect = $BarContainer/TimeBarContainer/TextureRect
+			time_texture.modulate = bar_color
+
+
+func _update_bar_positions() -> void:
+	var _bar_container: HBoxContainer = $BarContainer
+	var new_x: float = - (_bar_container.size.x / 2)
+	var new_y: float = radius + 10
+	_bar_container.position = Vector2(new_x, new_y)
+
+
+func _update_durability_bar(new_durability: int) -> void:
+	_durability_bar.value = new_durability
 #endregion
 
 
@@ -170,3 +213,11 @@ func _set_durability(value: int) -> void:
 	durability = value
 	durability_changed.emit(value)
 #endregion
+
+
+func _tween_time_bar() -> void:
+	if _time_bar_tween:
+		_time_bar_tween.kill()
+	_time_bar_tween = _time_bar.create_tween()
+	var end_value: float = _time_bar.max_value
+	_time_bar_tween.tween_property(_time_bar, "value", end_value, expiration_time)
