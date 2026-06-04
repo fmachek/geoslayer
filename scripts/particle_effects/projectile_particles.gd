944952b457e1eaeb1d381e177adc345d9f6c724e
fmachek <frantisekmachek2@gmail.com>
2026-06-04 20:35:30 +0200

    style: particle explosion particle tweaks

--- scripts/particle_effects/projectile_particles.gd
+++ scripts/particle_effects/projectile_particles.gd
@@ -11,6 +11,6 @@ func load_from_projectile(projectile: Projectile):
 	global_position = projectile.global_position
 	color = projectile.projectile_properties.draw_color
 	scale_amount_min = projectile.projectile_properties.radius * 0.8
-	scale_amount_max = scale_amount_min * 1.5
-	amount = projectile.projectile_properties.radius
+	scale_amount_max = scale_amount_min * 2.0
+	amount = projectile.projectile_properties.radius * 2
 	emitting = true
