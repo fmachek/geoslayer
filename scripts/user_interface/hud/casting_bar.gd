ce2af85a30e9c9b96ba4fdfdc26f5365c9d8bbc6
fmachek <frantisekmachek2@gmail.com>
2026-06-05 13:49:45 +0200

    fix: casting bar behavior
    Fix an issue where if the player held down both cast buttons and cast for example Lifesteal and Flurry, the casting bar would show up for the first ability, but not for the second one.

--- scripts/user_interface/hud/casting_bar.gd
+++ scripts/user_interface/hud/casting_bar.gd
@@ -3,6 +3,7 @@ extends ProgressBar
 
 @onready var label: Label = get_node("AbilityNameLabel")
 
+var _current_ability: Ability
 var _value_tween: Tween
 
 
@@ -16,17 +17,22 @@ func update_label(ability_name: String) -> void:
 
 func _on_player_spawned(player: PlayerCharacter) -> void:
 	player.started_casting.connect(_on_player_started_casting)
-	player.finished_casting.connect(_stop)
 
 
 func _on_player_started_casting(ability: Ability) -> void:
+	if _current_ability:
+		if _current_ability.finished_casting.is_connected(_stop.bind(_current_ability)):
+			_current_ability.finished_casting.disconnect(_stop.bind(_current_ability))
+	_current_ability = ability
+	ability.finished_casting.connect(_stop.bind(ability))
 	update_label(ability.ability_name)
 	_tween_value(ability.cast_time)
 	show()
 
 
-func _stop() -> void:
-	hide()
+func _stop(calling_ability: Ability) -> void:
+	if calling_ability == _current_ability:
+		hide()
 
 
 func _tween_value(cast_time: float) -> void:
