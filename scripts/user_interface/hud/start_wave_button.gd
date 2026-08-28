8254d354c02b0f72cf90492e2fa6c849f63f2adf
František Machek <frantisekmachek2@gmail.com>
2026-08-28 18:52:14 +0200

    Add wave number to start wave button
    The start wave button now says what wave will be started by pressing it.

--- scripts/user_interface/hud/start_wave_button.gd
+++ scripts/user_interface/hud/start_wave_button.gd
@@ -6,6 +6,16 @@ func _ready() -> void:
 	super()
 	pressed.connect(WorldManager._on_spawn_wave_button_pressed)
 	WorldManager.wave_started.connect(hide)
-	WorldManager.wave_ended.connect(show)
+	WorldManager.wave_ended.connect(show_and_update)
 	WorldManager.wave_ended.connect(_show_highlight)
 	WorldManager.final_wave_finished.connect(hide)
+	show_and_update.call_deferred()
+
+
+func show_and_update() -> void:
+	var world: World = WorldManager.current_world
+	var wave_manager: WaveManager = world.wave_manager
+	var next_wave: int = wave_manager.current_wave + 1
+	if next_wave <= wave_manager.max_waves:
+		text = "Start wave %d" % next_wave
+	show()
